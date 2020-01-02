4373361a35029847c0fdf40a4a9615bf3859c7c2
Pagas15 <57036145+Pagas15@users.noreply.github.com>
2020-01-02 18:39:22 +0200

    Add files via upload

--- SiteForPr4 (port)/js/main.js
+++ SiteForPr4 (port)/js/main.js
@@ -1,5 +1,11 @@
 
 $(document).ready(function(){
- 
   
 })
+  function slowScroll (id) {
+      var offset = 0;
+      $('html, body').animate ({
+          scrollTop: $(id).offset ().top - offset
+      }, 500)
+      return false
+  }
