42ef5ee1c5e2ee2c0d2b913cf7e247404c70eb85
Pagas15 <ghbdtn1104202@gmail.com>
2020-01-13 03:03:23 +0200

    fix mini and port

--- SiteForPr2 (mini)/js/main.js
+++ SiteForPr2 (mini)/js/main.js
@@ -1,25 +1,85 @@
+$(function(){
+  $('.menu-togle').click(function(){
+    $(this).toggleClass('active')
+    $('.menu').slideToggle(400)
+  })
+  $('.tabs a').click(function(){
+		$(this).parents('.tab-wrap').find('.tab-cont').addClass('hide');
+		$(this).parent().siblings().removeClass('active');
+		var id = $(this).attr('href');
+		$(id).removeClass('hide');
+		$(this).parent().addClass('active');
+		return false
+	});
 
-$(document).ready(function(){
-    // let loading = document.querySelector(".loading__text");
-    // let letters = loading.textContent.split("");
-    // loading.textContent = "";
-    // letters.forEach((letter, i) => {
-    //   let span = document.createElement("span");
-    //   span.textContent = letter;
-    //   span.style.animationDelay = `${i / 10}s`;
-    //   loading.append(span);
-    // });
-    // setTimeout(function(){ 
-    //     $('.loading').addClass('loading-hide')
-    //     setTimeout(function(){ 
-    //         $('.loading').hide(); 
-    //     }, 500);
-    // }, 2000);
-})
-function slowScroll (id) {
-  var offset = 0;
-  $('html, body').animate ({
-    scrollTop: $(id).offset ().top - offset
-  }, 500)
-  return false
-}
+  $('.banner-slider, .testimonial-slider').slick({
+		arrows: false,
+		dots: true,
+  });
+  
+  $('.portfolio-slider').slick({
+		dots: true,
+		appendArrows: '.portfolio-slider__buttons',
+		prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-chevron-left"></i></button>',
+		nextArrow: '<button type="button" class="slick-next"><i class="fa fa-chevron-right"></i></button>',
+		responsive: [
+	    {
+	      breakpoint: 767,
+	      settings: {
+	      	dots: false,
+	      }
+	    }
+	  ] 
+	});
+  
+  $nav_tabs_slider = $('.nav-tab-list');
+	settings = {
+		slidesToShow: 1,
+		prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-chevron-left"></i></button>',
+		nextArrow: '<button type="button" class="slick-next"><i class="fa fa-chevron-right"></i></button>',
+		infinite: false,
+  }
+  
+  
+  $nav_tabs_slider.on('afterChange', function(event, slick, currentSlide, nextSlide){
+		$(this).parents('.tab-wrap').find('.tab-cont').addClass('hide');
+		$(this).find('.slick-current').siblings().removeClass('active');
+		var id = $(this).find('.slick-current a').attr('href');
+		$(id).removeClass('hide');
+		$(this).find('.slick-current').addClass('active');
+		return false
+  })
+  
+  
+  $(window).on('resize load', function(){
+    if($(window).width() > 767) {
+      if($nav_tabs_slider.hasClass('slick-initialized')) {
+        $nav_tabs_slider.slick('unslick')
+      }
+      return
+    }
+    if(!$nav_tabs_slider.hasClass('slick-initialized')) {
+      return $nav_tabs_slider.slick(settings)
+    }
+  })
+});
+
+
+  function initMap() {
+    var pos = {lat: -37.806006, lng: 144.961291}, // Координаты центра карты
+      
+
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: pos,
+      zoom: 16,
+      disableDefaultUI: true,
+      scrollwheel: false,
+    });
+    marker = new google.maps.Marker({
+      position: pos,
+      map: map,
+      animation: google.maps.Animation.DROP,
+      icon: 'https://i.postimg.cc/bvFGdZKs/marker.png',
+    });
+  }
+  google.maps.event.addDomListener(window, 'load', initMap)
