cc5c8bdc4f06add80adcfb962be5a5ff39215d1e
Pagas15 <ghbdtn1104202@gmail.com>
2020-01-23 00:52:33 +0200

    add the effect paralaxadasdas

--- SiteForPr4 (port)/js/main.js
+++ SiteForPr4 (port)/js/main.js
@@ -1,1 +1,7 @@
 
+const parllax = document.querySelector(".headbox");
+
+window.addEventListener("scroll", function() {
+	let offset = window.pageYOffset;
+	parllax.style.backgroundPositionY = offset * 0.3 + 'px';
+});
--- js/main.js
+++
@@ -1,7 +0,0 @@
-
-const parllax = document.querySelector(".headbox");
-
-window.addEventListener("scroll", function() {
-	let offset = window.pageYOffset;
-	parllax.style.backgroundPositionY = offset * 0.3 + 'px';
-});
