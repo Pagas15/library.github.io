29a24d6ce91e2f3eab846b635463f846bc860798
Pagas15 <ghbdtn1104202@gmail.com>
2020-01-25 00:18:19 +0200

    add services

--- SiteForPr4 (port)/js/main.js
+++ SiteForPr4 (port)/js/main.js
@@ -1,14 +1,21 @@
 
-const parllax = document.querySelector(".headbox");
+    const parllax = document.querySelector(".headbox");
 
-window.addEventListener("scroll", function() {
-	let offset = window.pageYOffset;
-	parllax.style.backgroundPositionY = offset * 0.3 + 'px';
-});
+    window.addEventListener("scroll", function() {
+        let offset = window.pageYOffset;
+        parllax.style.backgroundPositionY = offset * 0.3 + 'px';
+    });
+
+    const paraaallax = document.querySelector(".services");
+
+    window.addEventListener("scroll", function() {
+        let offset = window.pageYOffset;
+        var pixtop = $(".headbox").height() + $(".project").height();
+        paraaallax.style.backgroundPositionY = (offset * 0.3) - (pixtop * 0.3) + 'px';
+    });
 
 
 
-	
 $(document).ready(function(){
 	$('#tooptip').on("click", function(){
 		$("span").attr("tooltip", "Успешно скопировано");
