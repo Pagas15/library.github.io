aedf0158046e678713f32a90dfd6e085f2336d36
Pagas15 <57036145+Pagas15@users.noreply.github.com>
2022-11-08 12:51:16 +0200

    update test

--- test/js/scripts.js
+++ test/js/scripts.js
@@ -1,3 +1,50 @@
+const tabs = (params) => {
+  const DEFAULTS = {
+    btns: 'data-tabs-btn',
+    blocks: 'data-tabs-block',
+    activeClass: 'active',
+  };
+
+  const objSet = { ...DEFAULTS, ...params };
+
+  const btns = document.querySelectorAll(`[${objSet.btns}]`),
+    tabs = document.querySelectorAll(`[${objSet.blocks}]`);
+
+  if (tabs && btns) {
+    const libsTabs = {};
+    tabs.forEach((tab) => {
+      libsTabs[tab.dataset.tabsBlock] = tab;
+    });
+
+    const clearOllTabs = () => {
+      tabs.forEach((item) => {
+        item.classList.remove(objSet.activeClass);
+        setTimeout(() => {
+          item.style.display = 'none';
+        }, 500);
+      });
+      btns.forEach((item) => item.classList.remove(objSet.activeClass));
+    };
+
+    const switchTab = (identifier, btn) => {
+      clearOllTabs();
+
+      setTimeout(() => {
+        libsTabs[identifier].style.display = 'block';
+        libsTabs[identifier].classList.add(objSet.activeClass);
+      }, 500);
+      btn && btn.classList.add(objSet.activeClass);
+    };
+
+    btns.forEach((btn) => {
+      const identifier = btn.dataset.tabsBtn;
+      btn.addEventListener('click', () => {
+        switchTab(identifier, btn);
+      });
+    });
+  }
+};
+
 const videoControl = () => {
   const videos = document.querySelectorAll('.videoContainer');
   if (videos) {
@@ -20,6 +67,7 @@ const videoControl = () => {
 
 const init = () => {
   videoControl();
+  tabs();
 };
 
 window.addEventListener('DOMContentLoaded', init);
