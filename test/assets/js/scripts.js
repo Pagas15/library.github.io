93cab050701cf94ba205157417819352a2b185e4
Pagas15 <57036145+Pagas15@users.noreply.github.com>
2022-11-13 15:56:27 +0200

    add popup

--- test/assets/js/scripts.js
+++ test/assets/js/scripts.js
@@ -85,9 +85,69 @@ const sliders = () => {
   }
 };
 
+const modal = () => {
+  const modal = document.querySelector('[data-modal-wrapper]');
+  if (modal) {
+    const modalOpens = document.querySelectorAll('[data-modal-btn-open]');
+    const modalClose = document.querySelectorAll('[data-modal-btn-close]');
+    const modalItems = modal.querySelectorAll('[data-modal-window]');
+
+    const obgListItems = {};
+    modalItems.forEach((item) => {
+      obgListItems[item.dataset.modalWindow] = item;
+    });
+
+    let nowOpenModal = null;
+
+    const closeOllModal = () => {
+      document.body.style.overflow = 'auto';
+      for (let key in obgListItems) {
+        obgListItems[key].classList.remove('active');
+      }
+    };
+    const closeModal = () => {
+      modal.classList.remove('active');
+      nowOpenModal = null;
+      closeOllModal();
+    };
+
+    const openModal = (identefier) => {
+      if (nowOpenModal === null) {
+        nowOpenModal = identefier;
+        obgListItems[identefier].classList.add('active');
+        modal.classList.add('active');
+        document.body.style.overflow = 'hidden';
+      } else if (nowOpenModal !== identefier) {
+        closeOllModal();
+        nowOpenModal = identefier;
+        obgListItems[identefier].classList.add('active');
+        document.body.style.overflow = 'hidden';
+      } else if (nowOpenModal === identefier) {
+        closeModal();
+      }
+    };
+
+    modalOpens.forEach((btn) => {
+      const identefier = btn.dataset.modalBtnOpen;
+      btn.addEventListener('click', (e) => {
+        openModal(identefier);
+      });
+    });
+
+    modalClose.forEach((btn) => {
+      btn.addEventListener('click', closeModal);
+    });
+
+    modal.addEventListener('click', (e) => {
+      e.target === modal && closeModal();
+    });
+  }
+};
+
 const init = () => {
   header();
   sliders();
+  modal();
 };
 
 window.addEventListener('DOMContentLoaded', init);
